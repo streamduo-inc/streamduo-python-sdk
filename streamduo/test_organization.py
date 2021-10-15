6e29ccf5320cb6314545c65d58c432f8c306f9f5
steve frensch <stevefrensch@steves-MacBook-Pro.local>
2021-10-15 13:19:56 -0400

    working behave

--- streamduo/test_organization.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from streamduo import auth
-from streamduo import organization
-
-class Test(TestCase):
-    def test_get_organization(self):
-        auth_manager = auth.AuthManager()
-        out = organization.get_organization(auth_manager, "asdf")
-        self.fail()
