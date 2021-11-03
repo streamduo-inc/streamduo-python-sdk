8ae3f1f1e3711621a0703fa0f3ce72e4635402cf
steve frensch <stevefrensch@steves-MacBook-Pro.local>
2021-11-03 12:51:10 -0400

    added unread records

--- streamduo/api/record.py
+++ streamduo/api/record.py
@@ -19,6 +19,13 @@ class Record:
         read_record_request.markAsRead = mark_as_read
         return self._read_record_request(stream_id, read_record_request)
 
+    def read_unread_records(self, stream_id, mark_as_read, record_count):
+        read_record_request = ReadRecordRequest()
+        read_record_request.readRecordRequestType = ReadRecordRequestType.UNREAD
+        read_record_request.markAsRead = mark_as_read
+        read_record_request.recordCount = record_count
+        return self._read_record_request(stream_id, read_record_request)
+
     def _read_record_request(self, stream_id, read_record_request):
         read_record_request.readRecordRequestType = read_record_request.readRecordRequestType.value
         return self.client.call_api("POST",
--- streamduo/api/test_record.py
+++ streamduo/api/test_record.py
@@ -42,3 +42,52 @@ class TestRecord(TestCase):
 
         ## Cleanup
         stream_controller.delete_stream(stream_id)
+
+
+    def test_unread_records(self):
+        display_name = 'test_stream'
+        stream_controller = Client(os.getenv('AUTH_CLIENT_ID'),
+                                   os.getenv('AUTH_CLIENT_SECRET')).get_stream_controller()
+        stream_request_result = stream_controller.create_stream(display_name)
+        stream_id = stream_request_result.json()['streamId']
+
+        record_controller = Client(os.getenv('AUTH_CLIENT_ID'),
+                                   os.getenv('AUTH_CLIENT_SECRET')).get_record_controller()
+        payload_1 = {'payload': 1}
+
+        payload_2 = {'payload': 2}
+
+        payload_3 = {'payload': 3}
+
+        record_id_1 = record_controller.write_record(stream_id, payload_1).json()['recordId']
+        record_id_2 = record_controller.write_record(stream_id, payload_2).json()['recordId']
+        record_id_3 = record_controller.write_record(stream_id, payload_3).json()['recordId']
+
+        read_unread_response = record_controller.read_unread_records(stream_id, False, 5)
+        assert len(read_unread_response.json()) == 3
+
+        read_one_response = record_controller.read_record(stream_id,record_id_2, True)
+
+        read_unread_response = record_controller.read_unread_records(stream_id, True, 5)
+        assert len(read_unread_response.json()) == 2
+
+        read_unread_response = record_controller.read_unread_records(stream_id, False, 5)
+        assert len(read_unread_response.json()) == 0
+
+        ## Cleanup
+        stream_controller.delete_stream(stream_id)
+
+    def test_write_record_errors(self):
+        record_controller = Client(os.getenv('AUTH_CLIENT_ID'),
+                                   os.getenv('AUTH_CLIENT_SECRET')).get_record_controller()
+
+        #stream does not exist
+        payload = {'level_1a': [{'level_2a': 99},
+                                {'level_2b': 'wayne'}],
+                   'level_1b': 'some text'
+                   }
+        write_response = record_controller.write_record("madeupsstreamid", payload)
+        print(write_response)
+        assert write_response.status_code == 401
+
+
