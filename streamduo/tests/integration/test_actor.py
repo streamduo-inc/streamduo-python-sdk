9859d9d2342a6dcf2ba400e65576c1c290ff1a9c
steve frensch <sfrensch@gmail.com>
2021-12-01 08:14:52 -0500

    cleanup

--- streamduo/tests/integration/test_actor.py
+++ streamduo/tests/integration/test_actor.py
@@ -6,11 +6,6 @@ from streamduo.client import Client
 
 class TestActor(TestCase):
 
-    # def test_initialize(self):
-    #     actor_controller = Client(os.getenv('AUTH_CLIENT_ID'), os.getenv('AUTH_CLIENT_SECRET')).get_actor_controller()
-    #     actor_controller.create_user()
-
-
     def test_create_machine_client(self):
 
         actor_controller = Client(os.getenv('AUTH_CLIENT_ID'), os.getenv('AUTH_CLIENT_SECRET')).get_actor_controller()
@@ -90,35 +85,3 @@ class TestActor(TestCase):
         # cleanup
         stream_controller.delete_stream(new_stream_id)
         assert actor_controller.get_machine_client(new_client_id).status_code == 404
-
-
-    # def test_too_many_machine_client(self):
-    #     actor_controller = Client(os.getenv('AUTH_CLIENT_ID'), os.getenv('AUTH_CLIENT_SECRET')).get_actor_controller()
-    #
-    #     #clear out list
-    #     client_list = actor_controller.get_clients()
-    #     for client in client_list.json():
-    #         print(client['clientId'])
-    #         actor_controller.delete_machine_client(client['clientId'])
-    #     client_list_new = actor_controller.get_clients()
-    #     assert len(client_list_new.json()) == 0
-    #
-    #     create_actor_response = actor_controller.create_machine_client("client display name test",
-    #                                                                    "client description test")
-    #     create_actor_response2 = actor_controller.create_machine_client("client display name test2",
-    #                                                                    "client description test2")
-    #     create_actor_response3 = actor_controller.create_machine_client("client display name test3",
-    #                                                                    "client description test3")
-    #     create_actor_response4 = actor_controller.create_machine_client("client display name test4",
-    #                                                                    "client description test4")
-    #     assert create_actor_response4.json()['message']  == "Max Number of Clients Exceeded"
-
-        # #clear out list
-        # client_list = actor_controller.get_clients()
-        # for client in client_list.json():
-        #     print(client['clientId'])
-        #     actor_controller.delete_machine_client(client['clientId'])
-        # client_list_new = actor_controller.get_clients()
-        # assert len(client_list_new.json()) == 0
-
-
