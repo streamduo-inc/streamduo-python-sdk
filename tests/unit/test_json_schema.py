b9090c85d1264c81ec4813b6acfed36ec68ef4ca
steve frensch <sfrensch@gmail.com>
2022-02-15 18:46:12 -0500

    working json validation

--- tests/unit/test_json_schema.py
+++ tests/unit/test_json_schema.py
@@ -18,9 +18,35 @@ class TestJson(TestCase):
             "Doors": "4"
         }
         assert val.validate_record(valid_rec)
-        inval_rec = valid_rec['extra'] = "xyz"
+        inval_rec = valid_rec.copy()
+        inval_rec['extra'] = "xyz"
         with self.assertRaises(jsonschema.exceptions.ValidationError):
             val.validate_record(inval_rec)
+
+    def test_json_list_validation(self):
+        val = JsonValidator()
+        val.set_local_schema("../test_schemas/car_schema.json")
+        valid_rec_1 = {
+            "Make": "Nissan",
+            "Price": "$9,700.00",
+            "Odometer (KM)": "31600",
+            "Colour": "White",
+            "Doors": "4"
+        }
+        valid_rec_2 = {
+            "Make": "Honda",
+            "Price": "$9,700.00",
+            "Odometer (KM)": "31600",
+            "Colour": "White",
+            "Doors": "4"
+        }
+        rec_list = [valid_rec_1, valid_rec_2]
+        assert val.validate_list(rec_list)
+
+        ## Make Bad
+        rec_list[1]['extra'] = "xyz"
+        with self.assertRaises(jsonschema.exceptions.ValidationError):
+            val.validate_list(rec_list)
 
     def test_bad_schema(self):
         val = JsonValidator()
