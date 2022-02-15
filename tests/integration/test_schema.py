8c8bc92fedb9c78c916bbfde6f94e17b1a3c306d
steve frensch <sfrensch@gmail.com>
2022-02-15 09:14:25 -0500

    passing del test

--- tests/integration/test_schema.py
+++ tests/integration/test_schema.py
@@ -3,6 +3,7 @@ import json
 from unittest import TestCase
 from streamduo.client import Client
 
+
 class TestStream(TestCase):
 
     def test_create_schema(self):
@@ -14,16 +15,30 @@ class TestStream(TestCase):
         new_schema_id = create_schema_response1.json()['schemaId']
         assert new_schema_id is not None
 
-        assert schema_controller.delete_schema(new_schema_id, "1").status_code == 200
+        ## Check if in schema is in user object
+        actor_controller = Client(os.getenv('AUTH_CLIENT_ID'), os.getenv('AUTH_CLIENT_SECRET')).get_actor_controller()
+        user_response = actor_controller.get_user()
+        found = False
+        for p in user_response.json()['schemaPermissionList']:
+            if p['schemaId'] == new_schema_id:
+                found = True
+                break
+        assert found == True
 
+        ## BAD Schema errors out
         car_schema['properties']['Make']['$id'] = "#root/Make 2"
         create_schema_response2 = schema_controller.create_schema(car_schema, "JSON")
         assert create_schema_response2.status_code == 400
 
-        # ## cleanup
-        # delete_response = actor_controller.delete_machine_client(new_client_id)
-        # assert delete_response.status_code == 200
-        #
-        # ## check get
-        # get_response = actor_controller.get_machine_client(new_client_id)
-        # assert get_response.status_code == 404
+        ## cleanup
+        delete_response = schema_controller.delete_schema(schema_id=new_schema_id, schema_version="1")
+        assert delete_response.status_code == 200
+
+        ## Check if schema is in user object
+        user_response_2 = actor_controller.get_user()
+        found = False
+        for p in user_response_2.json()['schemaPermissionList']:
+            if p['schemaId'] == new_schema_id:
+                found = True
+                break
+        assert found == False
