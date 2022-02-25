bfb653c3be090a9f1853fd3881748f583299e5c8
steve frensch <sfrensch@gmail.com>
2022-02-25 08:46:30 -0500

    rm build

--- build/lib/tests/test_avro.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from streamduo.validators.json_schema import Avro
-
-
-class TestAvro(TestCase):
-    def test_set_local_schema(self):
-        val = Avro()
-        val.set_local_schema("car_schema.avsc")
-        print(val.schema)
