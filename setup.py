2e5d39639659cf0af87434ff51c4911be08aaeae
steve frensch <sfrensch@gmail.com>
2022-05-10 10:27:08 -0400

    update requrements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ VERSION = "0.0.25"
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 
-REQUIRES = ["requests", "jsonschema"]
+REQUIRES = ["requests", "jsonschema", "pynacl", "pandas", "great_expectations"]
 
 setup(
     name=NAME,
